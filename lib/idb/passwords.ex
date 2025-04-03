0fdb0b0a9ec88a7c96563af3f2947eeace291ffe
Elkeid Me <elkeid.me@gmail.com>
2025-04-03 16:14:23 +0800

    Stage 2

--- lib/idb/passwords.ex
+++ lib/idb/passwords.ex
@@ -1,6 +1,7 @@
 defmodule Idb.Passwords do
-  alias Idb.Users
+  alias Idb.{Repo, Passwords, Users}
   use Ecto.Schema
+  import Ecto.Query
 
   schema "passwords" do
     field(:website, :string)
@@ -9,6 +10,13 @@ defmodule Idb.Passwords do
 
     belongs_to(:creator, Users, foreign_key: :creator_id)
     timestamps()
+  end
+
+  def creator(id) do
+    case Passwords |> where(id: ^id) |> select([:creator_id]) |> Repo.one() do
+      %{creator_id: creator} -> creator
+      nil -> nil
+    end
   end
 end
 
@@ -23,6 +31,77 @@ defmodule Idb.Passwords.List do
 
     data = Passwords |> where(creator_id: ^id) |> Repo.all()
     Utils.send_json(conn, data)
+  end
+end
+
+defmodule Idb.Passwords.Delete do
+  alias Idb.{Repo, Passwords, Utils}
+  import Ecto.Query
+
+  def init(options), do: options
+
+  def call(conn, _opts) do
+    user_id = Guardian.Plug.current_resource(conn)
+
+    with %{"id" => id} <- conn.body_params do
+      case Passwords.creator(id) do
+        nil ->
+          Utils.send_detail(conn, "不存在的条目。")
+
+        ^user_id ->
+          case Repo.delete(%Passwords{id: id}, id) do
+            {:ok, _} -> Utils.send_message(conn, "")
+            {:error, _} -> Utils.send_detail(conn, "数据库插入失败", 500)
+          end
+
+        _ ->
+          Utils.send_detail(conn, "不是该用户所属的条目")
+      end
+    else
+      _ -> Utils.send_detail(conn, "不合法的参数。")
+    end
+  end
+end
+
+defmodule Idb.Passwords.Edit do
+  alias Idb.{Repo, Passwords, Utils}
+  import Ecto.Query
+
+  def init(options), do: options
+
+  def call(conn, _opts) do
+    user_id = Guardian.Plug.current_resource(conn)
+
+    with %{"id" => id} <- conn.body_params do
+      case Passwords.creator(id) do
+        nil ->
+          Utils.send_detail(conn, "不存在的条目。")
+
+        ^user_id ->
+          attrs =
+            conn.body_params
+            |> Map.take(["website", "username", "password"])
+
+          try do
+            Passwords
+            |> where(id: ^id)
+            |> Repo.one()
+            |> Ecto.Changeset.cast(attrs, [:website, :username, :password])
+            |> Repo.update()
+
+            Utils.send_message(conn, "更新成功")
+          rescue
+            e ->
+              Utils.send_detail(conn, "更新失败")
+              IO.inspect(e)
+          end
+
+        _ ->
+          Utils.send_detail(conn, "不是该用户所属的条目")
+      end
+    else
+      _ -> Utils.send_detail(conn, "不合法的参数。")
+    end
   end
 end
 
@@ -42,7 +121,7 @@ defmodule Idb.Passwords.Add do
         {:error, _} -> Utils.send_detail(conn, "数据库插入失败", 500)
       end
     else
-      _ -> Utils.send_detail(conn, "Invalid parameters. 不合法的参数。")
+      _ -> Utils.send_detail(conn, "不合法的参数。")
     end
   end
 end
--- lib/idb/users.ex
+++ lib/idb/users.ex
@@ -25,7 +25,7 @@ defmodule Idb.Users do
 
   def hash_password(password, salt), do: :crypto.pbkdf2_hmac(:sha256, password, salt, 100_000, 32)
 
-  def verify_password(password, password_hashed, salt),
+  def password_verified?(password, password_hashed, salt),
     do: hash_password(password, salt) == password_hashed
 
   def id_exists?(id), do: Users |> where(id: ^id) |> Repo.exists?()
@@ -44,11 +44,11 @@ defmodule Idb.Users.Register do
         e in Ecto.ConstraintError ->
           case e.constraint do
             "users_email_index" ->
-              Utils.send_detail(conn, "Email already exists. 电子邮件已存在。")
+              Utils.send_detail(conn, "电子邮件已存在。")
           end
       end
     else
-      _ -> Utils.send_detail(conn, "Invalid parameters. 不合法的参数。")
+      _ -> Utils.send_detail(conn, "不合法的参数。")
     end
   end
 end
@@ -68,17 +68,17 @@ defmodule Idb.Users.Login do
              |> where(email: ^email)
              |> select([:password_hashed, :salt, :id])
              |> Repo.one() do
-        if Users.verify_password(password, password_hashed, salt) do
+        if Users.password_verified?(password, password_hashed, salt) do
           Utils.send_jwt(conn, id)
         else
-          Utils.send_detail(conn, "Email or password is wrong. 电子邮件或密码错误。")
+          Utils.send_detail(conn, "电子邮件或密码错误。")
         end
       else
         _ ->
-          Utils.send_detail(conn, "Email or password is wrong. 电子邮件或密码错误。")
+          Utils.send_detail(conn, "电子邮件或密码错误。")
       end
     else
-      _ -> Utils.send_detail(conn, "Invalid parameters. 不合法的参数。")
+      _ -> Utils.send_detail(conn, "不合法的参数。")
     end
   end
 end
